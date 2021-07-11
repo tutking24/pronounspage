72fdc498d0e4ae4906f4cdecaa4a2c5a42f476d7
Avris <andre@avris.it>
2021-07-11 12:17:33 +0200

    #220 [profile] card images - optimise screenshots (reuse the same browser instance for a chunk of profiles)

--- server/cards.js
+++ server/cards.js
@@ -14,36 +14,71 @@ for (let [code, , url, ] of locales) {
     urlBases[code] = url + '/card/@'; // 'http://localhost:3000/card/@'
 }
 
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+const modes = ['light', 'dark'];
+
 (async () => {
     const db = await dbConnection();
-    for (let {id, locale, username} of await db.all('SELECT profiles.id, profiles.locale, users.username FROM profiles LEFT JOIN users on profiles.userId = users.id WHERE profiles.card IS NULL')) {
-        if (isHighLoadTime(locale)) {
+    while (true) {
+        const profiles = (await db.all(`
+            SELECT profiles.id, profiles.locale, users.username
+            FROM profiles
+                     LEFT JOIN users on profiles.userId = users.id
+            WHERE profiles.card IS NULL
+            ORDER BY RANDOM()
+            LIMIT 16
+        `)).filter(({locale}) => !isHighLoadTime(locale));
+
+        if (profiles.length === 0) {
+            console.log('No profiles in the queue');
+            await sleep(5000);
             continue;
         }
-        // if (locale !== 'pl' || username !== 'andrea') { continue; }
 
-        const cardId = ulid();
-        const key = `card/${locale}/${username}-${cardId}.png`;
-
-        console.log(locale, username, cardId);
-
-        for (let dark of [false, true]) {
-            const [ buffer ] = await new Pageres({
-                darkMode: dark,
-                delay: 3,
-            })
-                .src(urlBases[locale] + username, ['1024x300'])
-                .run();
-
-            const s3putResponse = await s3.putObject({
-                Key: dark ? key.replace('.png', '-dark.png') : key,
-                Body: buffer,
-                ContentType: 'image/png',
-                ACL: 'public-read',
-            }).promise();
+        const results = {
+            'light': {},
+            'dark': {},
         }
 
-        await db.get(`UPDATE profiles SET card='https://${awsConfig.params.Bucket}.s3.${awsConfig.region}.amazonaws.com/${key}' WHERE id='${id}'`)
-        // '/card/@${username}.png'
+        try {
+            for (let mode of modes) {
+                const pr = new Pageres({
+                    darkMode: mode === 'dark',
+                    delay: 3,
+                });
+
+                for (let {locale, username} of profiles) {
+                    console.log(`Shooting @${username} (${locale}, ${mode})`);
+                    pr.src(urlBases[locale] + username, ['1024x300'])
+                }
+
+                for (let buffer of await pr.run()) {
+                    const username = buffer.filename.match(/!card!@(.*)-1024x300\.png/)[1];
+                    results[mode][username] = buffer;
+                }
+            }
+        } catch (e) {
+            console.error(e);
+            continue;
+        }
+
+        for (let {id, locale, username} of profiles) {
+            const cardId = ulid();
+            const key = `card/${locale}/${username}-${cardId}.png`;
+            for (let mode of modes) {
+                console.log(`Uploading @${username} (${locale}, ${mode}) – ${cardId}`);
+                const buffer = results[mode][username];
+
+                const s3putResponse = await s3.putObject({
+                    Key: mode === 'dark' ? key.replace('.png', '-dark.png') : key,
+                    Body: buffer,
+                    ContentType: 'image/png',
+                    ACL: 'public-read',
+                }).promise();
+            }
+
+            await db.get(`UPDATE profiles SET card='https://${awsConfig.params.Bucket}.s3.${awsConfig.region}.amazonaws.com/${key}' WHERE id='${id}'`)
+        }
     }
 })();
