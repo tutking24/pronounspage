9ad1636249d7fac265a8079290a1be57a98d0778
Avris <andre@avris.it>
2021-07-26 20:22:35 +0200

    [de] wip

--- locale/de/pronouns/GrammarTables.vue
+++ locale/de/pronouns/GrammarTables.vue
@@ -9,20 +9,27 @@
             <table class="table table-sm">
                 <thead>
                 <tr>
+                    <th></th>
                     <th>Nominativ</th>
                     <th>Dativ</th>
                     <th>Akkusativ</th>
                     <th>Genitiv</th>
-                    <th>Artikel</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
+                    <th>Pronomen</th>
                     <td><Morpheme :pronoun="selectedPronoun" morpheme="pronoun_n" :counter="counter"/></td>
+                    <td><Morpheme :pronoun="selectedPronoun" morpheme="pronoun_g" :counter="counter"/></td>
                     <td><Morpheme :pronoun="selectedPronoun" morpheme="pronoun_d" :counter="counter"/></td>
                     <td><Morpheme :pronoun="selectedPronoun" morpheme="pronoun_a" :counter="counter"/></td>
-                    <td><Morpheme :pronoun="selectedPronoun" morpheme="pronoun_g" :counter="counter"/></td>
+                </tr>
+                <tr>
+                    <th>Artikel</th>
                     <td><Morpheme :pronoun="selectedPronoun" morpheme="article_n" :counter="counter"/></td>
+                    <td><Morpheme :pronoun="selectedPronoun" morpheme="article_g" :counter="counter"/></td>
+                    <td><Morpheme :pronoun="selectedPronoun" morpheme="article_d" :counter="counter"/></td>
+                    <td><Morpheme :pronoun="selectedPronoun" morpheme="article_a" :counter="counter"/></td>
                 </tr>
                 </tbody>
             </table>
