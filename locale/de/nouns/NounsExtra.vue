6684108bb4c1951a6880f77864700c8e6925818d
Frederike  Klee <freddy27@hotmail.de>
2021-08-03 16:51:29 +0000

    [de][pronouns] coorected 'Autor'-forms.Added words

--- locale/de/nouns/NounsExtra.vue
+++ locale/de/nouns/NounsExtra.vue
@@ -38,28 +38,28 @@
             return {
                 declensions: {
                     'Y-Formen': new NounDeclension({
-                        N: 'das Authory', G: 'des Authorys', D: 'dem Authory', A: 'das Authory',
-                        N_pl: 'die Authorys', G_pl: 'der Authorys', D_pl: 'den Authorys', A_pl: 'die Authorys',
+                        N: 'das Autory', G: 'des Autorys', D: 'dem Autory', A: 'das Autory',
+                        N_pl: 'die Autorys', G_pl: 'der Autorys', D_pl: 'den Autorys', A_pl: 'die Autorys',
                     }),
                     'Inklusivum': new NounDeclension({
-                        N: 'de Authore', G: 'dern Authore', D: 'dern Authore', A: 'de Authore',
-                        N_pl: 'die Authorne', G_pl: 'der Authorne', D_pl: 'den Authorne', A_pl: 'die Authorne',
+                        N: 'de Autore', G: 'dern Autore', D: 'dern Autore', A: 'de Autore',
+                        N_pl: 'die Autorne', G_pl: 'der Autorne', D_pl: 'den Autorne', A_pl: 'die Autorne',
                     }),
                     'Doppelpunkt-Formen': new NounDeclension({
-                        N: 'der:die Author:in', G: 'des:der Authors:in', D: 'dem:der Author:in', A: 'den:die Author:in',
-                        N_pl: 'die Author:innen', G_pl: 'der Author:innen', D_pl: 'den Author:innen', A_pl: 'die Author:innen',
+                        N: 'der:die Autor:in', G: 'des:der Autors:in', D: 'dem:der Autor:in', A: 'den:die Autor:in',
+                        N_pl: 'die Autor:innen', G_pl: 'der Autor:innen', D_pl: 'den Autor:innen', A_pl: 'die Autor:innen',
                     }),
                     'Unterstrich-Formen': new NounDeclension({
-                        N: 'der_die Author_in', G: 'des_der Authors_in', D: 'dem_der Author_in', A: 'den_die Author_in',
-                        N_pl: 'die Author_innen', G_pl: 'der Author_innen', D_pl: 'den Author_innen', A_pl: 'die Author_innen',
+                        N: 'der_die Autor_in', G: 'des_der Autors_in', D: 'dem_der Autor_in', A: 'den_die Autor_in',
+                        N_pl: 'die Autor_innen', G_pl: 'der Autor_innen', D_pl: 'den Autor_innen', A_pl: 'die Autor_innen',
                     }),
                     'Sternzeichen-Formen': new NounDeclension({
-                        N: 'der*die Author*in', G: 'des*der Authors*in', D: 'dem*der Author*in', A: 'den*die Author*in',
-                        N_pl: 'die Author*innen', G_pl: 'der Author*innen', D_pl: 'den Author*innen', A_pl: 'die Author*innen',
+                        N: 'der*die Autor*in', G: 'des*der Autors*in', D: 'dem*der Autor*in', A: 'den*die Autor*in',
+                        N_pl: 'die Autor*innen', G_pl: 'der Autor*innen', D_pl: 'den Autor*innen', A_pl: 'die Autor*innen',
                     }),
                     'Großbuchstabe-Formen': new NounDeclension({
-                        N: '?? AuthorIn', G: '?? AuthorsIn', D: '?? AuthorIn', A: '?? AuthorIn',
-                        N_pl: 'die AuthorInnen', G_pl: 'der AuthorInnen', D_pl: 'den AuthorInnen', A_pl: 'die AuthorInnen',
+                        N: 'derDie AutorIn', G: 'desDer AutorIn', D: 'demDer AutorIn', A: 'denDie AutorIn',
+                        N_pl: 'die AutorInnen', G_pl: 'der AutorInnen', D_pl: 'den AutorInnen', A_pl: 'die AutorInnen',
                     }),
                 },
             }
