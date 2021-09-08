51b9580b04965298ed87429bc0c2b5568aa55139
Andrea Vos <andrea@avris.it>
2021-09-08 22:57:31 +0200

    [support] add BTC

--- components/Support.vue
+++ components/Support.vue
@@ -6,7 +6,7 @@
         </h3>
         <p><T>support.description</T></p>
         <p>
-            <a v-for="link in config.support.links" :key="link.url"
+            <a v-for="link in [...links, ...config.support.links]" :key="link.url"
                :href="link.url" target="_blank" rel="noopener"
                class="btn btn-outline-primary border m-1">
                 <Icon :v="link.icon" :set="link.iconSet || 'l'"/>
@@ -15,3 +15,17 @@
         </p>
     </section>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                links: [
+                    {headline: 'Ko-Fi', url: 'https://ko-fi.com/radajezykaneutralnego', icon: 'coffee', },
+                    {headline: 'PayPal', url: 'https://paypal.me/RJNeutralnego', icon: 'paypal', iconSet: 'b'},
+                    {headline: 'BTC', url: 'https://www.blockchain.com/btc/address/1GFVeVZfYtPqDMAoCwK8WT2C4HjwiGxXS1', icon: 'bitcoin', iconSet: 'b'},
+                ]
+            }
+        }
+    }
+</script>
