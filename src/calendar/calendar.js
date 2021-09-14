278eeeebf9f8a22be918b25ccafa092cffb7d5cb
Andrea Vos <andrea@avris.it>
2021-09-14 18:38:17 +0000

    [calendar] enable english

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,6 +1,6 @@
 import { Day, Calendar } from './helpers';
 import internationalEvents from '../../locale/_/events';
-import localEvents from '../../locale/pl/events';
+import localEvents from '../../data/events';
 
 export const currentYear = new Calendar(
     Day.today().year,
