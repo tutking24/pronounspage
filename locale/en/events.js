d1ac253e38adc7a2d2120d043f2f3307fa84046d
Avris <andre@avris.it>
2021-09-14 14:12:17 +0200

    [calendar] enable english

--- locale/en/events.js
+++ locale/en/events.js
@@ -1,5 +1,7 @@
-import {Event, day, week, month, dayYear} from "../../src/calendar/helpers";
+import {Event, day, week, month, dayYear, EventLevel} from "../../src/calendar/helpers";
 
 export default [
-
+    new Event('LGBT History Month (UK)', 'Progress Pride', 2, month, EventLevel.Month),
+    new Event('LGBT History Month (US/Canada/Australia)', 'Progress Pride', 10, month, EventLevel.Month),
+    new Event('LGBT History Month (Ireland)', 'Progress Pride', 5, month, EventLevel.Month),
 ];
