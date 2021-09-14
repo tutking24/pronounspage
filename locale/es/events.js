dc96bec14b914ced9e3a3cd47231285778e3e7ed
Avris <andre@avris.it>
2021-09-14 14:21:20 +0200

    [es][calendar] add history month

--- locale/es/events.js
+++ locale/es/events.js
@@ -1,5 +1,5 @@
-import {Event, day, week, month, dayYear} from "../../src/calendar/helpers";
+import {Event, day, week, month, dayYear, EventLevel} from "../../src/calendar/helpers";
 
 export default [
-
+    new Event('Mes de la Historia LGBT (EE.UU.)', 'Progress Pride', 10, month, EventLevel.Month),
 ];
