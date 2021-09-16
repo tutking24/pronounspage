b48a10567c2f54080fa0b471bc467c7bb109d025
Andrea Vos <andrea@avris.it>
2021-09-16 03:36:49 +0000

    [bug] fix pt

--- locale/pt/events.js
+++ locale/pt/events.js
@@ -1,9 +1,8 @@
-import {Event, day, week, month, dayYear} from "../../src/calendar/helpers";
+import {Event, day, week, month, dayYear, EventLevel} from "../../src/calendar/helpers";
 
 export default [
 
     // static date
     new Event('Dia da Igualdade Matrimonial (Portugal)', null, 6, day(5), EventLevel.MajorDay),
     new Event('Dia da Igualdade Matrimonial (Brasil)', null, 5, day(16), EventLevel.MajorDay),
-  
 ];
