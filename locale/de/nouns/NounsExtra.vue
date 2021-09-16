0224ec088daed2e75366cb0fbc2fe7dede2da245
Andrea Vos <andrea@avris.it>
2021-09-16 01:16:06 +0000

    [de] Autor* -> Arbeiter*

--- locale/de/nouns/NounsExtra.vue
+++ locale/de/nouns/NounsExtra.vue
@@ -56,8 +56,8 @@
                 declensions: {
                     'Y-Formen': {
                         declension: new NounDeclension({
-                            N: 'das Autory', G: 'des Autorys', D: 'dem Autory', A: 'das Autory',
-                            N_pl: 'die Autorys', G_pl: 'der Autorys', D_pl: 'den Autorys', A_pl: 'die Autorys',
+                            N: 'das Arbeitery', G: 'des Arbeiterys', D: 'dem Arbeitery', A: 'das Arbeitery',
+                            N_pl: 'die Arbeiterys', G_pl: 'der Arbeiterys', D_pl: 'den Arbeiterys', A_pl: 'die Arbeiterys',
                         }),
                         extendedDeclension: new NounDeclension({
                             N: 'mein bestes Lehry', G: 'meines besten Lehrys', D: 'meinem besten Lehry', A: 'mein bestes Lehry',
@@ -67,8 +67,8 @@
                     'Inklusivum': {
                         info: 'Formen vorgestellt vom {https://geschlechtsneutral.net/=Verein für geschlechtsneutrales Deutsch}.',
                         declension: new NounDeclension({
-                            N: 'de Autore', G: 'dern Autores', D: 'dern Autore', A: 'de Autore',
-                            N_pl: 'die Autorerne', G_pl: 'der Autorerne', D_pl: 'den Autorernen', A_pl: 'die Autorerne',
+                            N: 'de Arbeitere', G: 'dern Arbeiteres', D: 'dern Arbeitere', A: 'de Arbeitere',
+                            N_pl: 'die Arbeitererne', G_pl: 'der Arbeitererne', D_pl: 'den Arbeiterernen', A_pl: 'die Arbeitererne',
                         }),
                         extendedDeclension: new NounDeclension({
                             N: 'mein beste Lehrere', G: 'meinern besten Lehreres', D: 'meinern besten Lehrere', A: 'mein beste Lehrere',
@@ -77,7 +77,7 @@
                     },
                     'Ojum': {
                         info: `
-                            Formen {https://www.frumble.de/blog/2021/03/26/ueberlegungen-zu-einer-genderneutralen-deutschen-grammatik=vorgestellt von Frumble}.<br/><br/>
+                            Formen vorgestellt von {https://www.frumble.de/blog/2021/03/26/ueberlegungen-zu-einer-genderneutralen-deutschen-grammatik=Frumble}.<br/><br/>
                             Siehe auch: {/oj=Neopronomen „oj/ojm“}.<br/><br/>
                             Die Idee ist, die theoretisch unspezifischen Formen auf -t, -ent, -ant und -or im Singular nicht durchzugendern:
                             Wird das Ojum bei -er statt des generischen Maskulinums der Standard, verändert sich mittelfristig voraussichtlich
@@ -85,8 +85,8 @@
                             und die gewohnten Kurzformen erhalten zu können. Trotzdem ist kurzfristig auch immer explizite Singular-Genderung mit -u freigestellt.
                         `,
                         declension: new NounDeclension({
-                            N: 'dej Autu', G: 'dejs Autus', D: 'dojm Autu', A: 'dojn Autu',
-                            N_pl: 'die Autoroj', G_pl: 'der Autoroj', D_pl: 'den Autorojn', A_pl: 'die Autoroj',
+                            N: 'dej Arbeitu', G: 'dejs Arbeitus', D: 'dojm Arbeitu', A: 'dojn Arbeitu',
+                            N_pl: 'die Arbeiteroj', G_pl: 'der Arbeiteroj', D_pl: 'den Arbeiterojn', A_pl: 'die Arbeiteroj',
                         }),
                         extendedDeclension: new NounDeclension({
                             N: 'meinu bestu Lehru', G: 'meinus besten Lehrus', D: 'meinum besten Lehru', A: 'meinun bestu Lehru',
@@ -95,8 +95,8 @@
                     },
                     'Doppelpunkt-Formen': {
                         declension: new NounDeclension({
-                            N: 'der:die Autor:in', G: 'des:der Autors:in', D: 'dem:der Autor:in', A: 'den:die Autor:in',
-                            N_pl: 'die Autor:innen', G_pl: 'der Autor:innen', D_pl: 'den Autor:innen', A_pl: 'die Autor:innen',
+                            N: 'der:die Arbeiter:in', G: 'des:der Arbeiters:in', D: 'dem:der Arbeiter:in', A: 'den:die Arbeiter:in',
+                            N_pl: 'die Arbeiter:innen', G_pl: 'der Arbeiter:innen', D_pl: 'den Arbeiter:innen', A_pl: 'die Arbeiter:innen',
                         }),
                         extendedDeclension: new NounDeclension({
                             N: 'mein:e beste:r Lehrer:in', G: 'meines:r besten Lehrer:in', D: 'meinem:r besten Lehrer:in', A: 'meine:n beste:n Lehrer:in',
@@ -105,8 +105,8 @@
                     },
                     'Unterstrich-Formen': {
                         declension: new NounDeclension({
-                            N: 'der_die Autor_in', G: 'des_der Autors_in', D: 'dem_der Autor_in', A: 'den_die Autor_in',
-                            N_pl: 'die Autor_innen', G_pl: 'der Autor_innen', D_pl: 'den Autor_innen', A_pl: 'die Autor_innen',
+                            N: 'der_die Arbeiter_in', G: 'des_der Arbeiters_in', D: 'dem_der Arbeiter_in', A: 'den_die Arbeiter_in',
+                            N_pl: 'die Arbeiter_innen', G_pl: 'der Arbeiter_innen', D_pl: 'den Arbeiter_innen', A_pl: 'die Arbeiter_innen',
                         }),
                         extendedDeclension: new NounDeclension({
                             N: 'mein_e beste_r Lehrer_in', G: 'meines_r besten Lehrer_in', D: 'meinem_r besten Lehrer_in', A: 'meine_n beste_n Lehrer_in',
@@ -115,8 +115,8 @@
                     },
                     'Sternchen-Formen': {
                         declension: new NounDeclension({
-                            N: 'der*die Autor*in', G: 'des*der Autors*in', D: 'dem*der Autor*in', A: 'den*die Autor*in',
-                            N_pl: 'die Autor*innen', G_pl: 'der Autor*innen', D_pl: 'den Autor*innen', A_pl: 'die Autor*innen',
+                            N: 'der*die Arbeiter*in', G: 'des*der Arbeiters*in', D: 'dem*der Arbeiter*in', A: 'den*die Arbeiter*in',
+                            N_pl: 'die Arbeiter*innen', G_pl: 'der Arbeiter*innen', D_pl: 'den Arbeiter*innen', A_pl: 'die Arbeiter*innen',
                         }),
                         extendedDeclension: new NounDeclension({
                             N: 'mein*e beste*r Lehrer*in', G: 'meines*r besten Lehrer*in', D: 'meinem*r besten Lehrer*in', A: 'meine*n beste*n Lehrer*in',
@@ -124,10 +124,10 @@
                         }),
                     },
                     'Großbuchstabe-Formen': {
-                        warning: 'Formen wie z.B. „AutorIn“ oder „LehrerInnen“. Das Binnen-I bezieht sich nur auf die männliche und die weibliche Form der Wörter und schließt damit (wie das generische Maskulinium) immer noch sehr viele Menschen aus der Sprache aus. Deshalb ist diese Variante genauso wenig zu empfehlen wie das generische Maskulinum oder Femininum.',
+                        warning: 'Formen wie z.B. „ArbeiterIn“ oder „LehrerInnen“. Das Binnen-I bezieht sich nur auf die männliche und die weibliche Form der Wörter und schließt damit (wie das generische Maskulinium) immer noch sehr viele Menschen aus der Sprache aus. Deshalb ist diese Variante genauso wenig zu empfehlen wie das generische Maskulinum oder Femininum.',
                         // declension: new NounDeclension({
-                        //     N: 'derDie AutorIn', G: 'desDer AutorIn', D: 'demDer AutorIn', A: 'denDie AutorIn',
-                        //     N_pl: 'die AutorInnen', G_pl: 'der AutorInnen', D_pl: 'den AutorInnen', A_pl: 'die AutorInnen',
+                        //     N: 'derDie ArbeiterIn', G: 'desDer ArbeiterIn', D: 'demDer ArbeiterIn', A: 'denDie ArbeiterIn',
+                        //     N_pl: 'die ArbeiterInnen', G_pl: 'der ArbeiterInnen', D_pl: 'den ArbeiterInnen', A_pl: 'die ArbeiterInnen',
                         // }),
                         // extendedDeclension: new NounDeclension({
                         //     N: 'meinE besteR LehrerIn', G: 'meinesR besten LehrerIn', D: 'meinemR besten LehrerIn', A: 'meineN besteN LehrerIn',
