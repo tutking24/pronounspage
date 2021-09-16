b66888e44112368f3d34ba6be693e419edb5360b
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-09-16 02:35:57 +0000

    [pt] Update events.js

--- locale/pt/events.js
+++ locale/pt/events.js
@@ -2,4 +2,8 @@ import {Event, day, week, month, dayYear} from "../../src/calendar/helpers";
 
 export default [
 
+    // static date
+    new Event('Dia da Igualdade Matrimonial (Portugal)', null, 6, day(5), EventLevel.MajorDay),
+    new Event('Dia da Igualdade Matrimonial (Brasil)', null, 5, day(16), EventLevel.MajorDay),
+  
 ];
