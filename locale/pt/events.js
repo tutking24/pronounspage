ce4d7a5102b9ad5dd390241f2a466ec2f8135ffa
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-09-17 21:03:46 +0000

    [pt] Update events.js

--- locale/pt/events.js
+++ locale/pt/events.js
@@ -11,7 +11,6 @@ module.exports = [
     new Event('Dia da Igualdade Matrimonial (Portugal)', null, 6, day(5), EventLevel.MajorDay),
     new Event('Dia da Igualdade Matrimonial (Brasil)', null, 5, day(16), EventLevel.MajorDay),
     new Event('Dia Nacional da Visibilidade Trans (Brasil)', 'Transgender', 1, day(29), EventLevel.MajorDay),
-    new Event('Dia de Luta e Resistência de Pessoas Transmasculinas (Brasil)', 'Transmasculine', 2, day(20), EventLevel.MajorDay),
     new Event('Dia Nacional do Orgulho Gay (Brasil)', 'LGBTQ', 3, day(25), EventLevel.MajorDay),
     new Event('Dia Nacional da Visibilidade Lésbica (Brasil)', 'Lesbian', 8, day(29), EventLevel.MajorDay),
     new Event('Aniversário da morte de Marielle Franco', null, 3, day(14), EventLevel.MajorDay),
