be11b799cb2c157c1eb2ac59e0f356ddb07b0a42
Avris <andre@avris.it>
2021-09-18 22:49:17 +0200

    [de][calendar] fix banner

--- locale/de/events.js
+++ locale/de/events.js
@@ -1,6 +1,6 @@
+const {Event, day, week, month, dayYear, EventLevel} = require("../../src/calendar/helpers");
+
 module.exports = [
-
-// months
-new Event('{https://queerhistory.de=Queer History Month}', 'Progress Pride', 5, month, EventLevel.Month),
-
+    // months
+    new Event('{https://queerhistory.de=Queer History Month}', 'Progress Pride', 5, month, EventLevel.Month),
 ];
