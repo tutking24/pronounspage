0f53af25094db0c4f19c222d1486ea980c798b85
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-09-19 14:56:12 +0000

    [de] Update events.js

--- locale/de/events.js
+++ locale/de/events.js
@@ -3,4 +3,9 @@ const {Event, day, week, month, dayYear, EventLevel} = require("../../src/calend
 module.exports = [
     // months
     new Event('{https://queerhistory.de=Queer History Month}', 'Progress Pride', 5, month, EventLevel.Month),
+    new Event('Jahrestag der Ehe für alle in Deutschland', null, 10, day(1), EventLevel.MajorDay),
+    new Event('Jahrestag der Ehe für alle in Österreich', null, 1, day(1), EventLevel.MajorDay),
+    new Event('Jahrestag der Ehe für alle in Belgien', null, 6, day(1), EventLevel.MajorDay),
+    new Event('Jahrestag der Ehe für alle in Luxemburg', null, 1, day(1), EventLevel.MajorDay),
+
 ];
