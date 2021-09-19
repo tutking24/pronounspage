c52f5c75c1447adbeff364849d340609cfe21391
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-09-19 15:51:42 +0000

    Update events.js

--- locale/_/events.js
+++ locale/_/events.js
@@ -49,6 +49,7 @@ module.exports = [
     new Event('transmasculine_day', 'Transmasculine', 2, day(20), EventLevel.MajorDay),
     new Event('asexual_visibility_day', 'Asexual', 5, day(8), EventLevel.MajorDay),
     new Event('holocaust_remembrance_day', null, 1, day(27), EventLevel.MajorDay),
+    new Event('black_ribbon_day', null, 8, day(23), EventLevel.MajorDay),
 
     // dynamic date
     new Event('arospec_week', 'Aromantic', 2, week(function* (monthDays) {
