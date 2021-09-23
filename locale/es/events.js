f55bf67d8a3e7b7a2b14f6f2fcfec27d3b2d9849
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-09-23 21:30:06 +0000

    [es] Update events.js

--- locale/es/events.js
+++ locale/es/events.js
@@ -19,7 +19,7 @@ module.exports = [
     new Event('Día de las Rebeldías Lésbicas', 'Lesbian_', 10, day(13), EventLevel.MajorDay),
     new Event('Día de la Promoción de los Derechos de las Personas Trans (Argentina)', 'Transgender', 3, day(18), EventLevel.MajorDay),   new Event('Aniversario de la Sanción de la Ley de Identidad de Género en Argentina', 'Transgender', 5, day(9), EventLevel.MajorDay),
     new Event('Aniversario del {https://es.wikipedia.org/wiki/Baile_de_los_cuarenta_y_uno=Baile de los Cuarenta y Uno} (México)', null, 11, day(18), EventLevel.MajorDay),
-    new Event('Vela de la Comunidad Muxe (Oaxaca)', 'Muxe', 11, day(15), EventLevel.MajorDay),
+    new Event('Día de la Comunidad Muxe (Vela de las Intrépidas)', 'Muxe', 11, day(15), EventLevel.MajorDay),
         new Event('Día Nacional de la Libertad para Casarse (EE.UU.)', null, 2, day(12), EventLevel.MajorDay),
     new Event('Día Nacional de la Prueba del VIH en Personas Transgénero (EE.UU.)', 'Transgender', 4, day(18), EventLevel.MajorDay),
     new Event('Día Nacional de la Prueba del VIH (EE.UU.)', null, 6, day(27), EventLevel.MajorDay),
