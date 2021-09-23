c2873a3501c8ee68e94b3217c2e9174cf2d74a9f
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-09-23 21:31:16 +0000

    [en] Update events.js

--- locale/en/events.js
+++ locale/en/events.js
@@ -28,6 +28,7 @@ module.exports = [
     new Event('National HIV/AIDS and Aging Awareness Day (US)', null, 9, day(18), EventLevel.MajorDay),
     new Event('LGBT Center Awareness Day (US)', null, 10, day(19), EventLevel.MajorDay),
     new Event('{https://twitter.com/_EQUALGROUND_/status/1440232964286124050=Lesbian Visibility Day} (Sri Lanka)', 'Lesbian', 9, day(21), EventLevel.MajorDay),
+    new Event('Muxe Awareness Day (Vela de las Intrépidas)', 'Muxe', 11, day(15), EventLevel.MajorDay),
 
     // dynamic date
     new Event('Wear it Purple Day (Australia)', null, 8, function* (monthDays) {
