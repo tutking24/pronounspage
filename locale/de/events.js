e6b2d9a520761bc783f71e233191f953da356072
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-09-27 18:47:30 +0000

    [de] Update events.js

--- locale/de/events.js
+++ locale/de/events.js
@@ -10,5 +10,8 @@ module.exports = [
 
     // one-off events
     new Event('Ehe für alle in der Schweiz', 7, dayYear(1, 2022), EventLevel.Day),
+    new Event('{https://www.csd-d.de/de/=CSD Düsseldorf}', 10, dayYear(16, 2021), EventLevel.Day),
+    new Event('{https://csd-bielefeld.de/=CSD Bielefeld}', 10, dayYear(2, 2021), EventLevel.Day),
+    new Event('{https://www.facebook.com/events/133873485389727=CSD Erfurt}', 10, dayYear(9, 2021), EventLevel.Day),
 
 ];
