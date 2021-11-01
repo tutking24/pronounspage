496587d8d545a689cef17e60f71cb5ccd326d9e7
Andrea Vos <andrea@avris.it>
2021-11-01 21:43:45 +0100

    [pl][blog] petycja spellcheck

--- src/parseMarkdown.js
+++ src/parseMarkdown.js
@@ -6,7 +6,7 @@ export default async function parseMarkdown(markdown) {
                 .replace(/<\/table>/g, '</table></div>')
                 .replace(/{favicon=(.+?)}/g, '<img src="https://$1" alt="Favicon" style="width: 1em; height: 1em;"/>')
                 .replace(/<a href="http/g, '<a target="_blank" rel="noopener" href="http')
-                .replace(/<p>{details=(.+?)}<\/p>(.+?)<p>{\/details}<\/p>/gms, '<details class="border mb-3"><summary class="bg-light p-3">$1</summary><div class="border-top p-3">$2</div></details>')
+                .replace(/<p>{details=(.+?)}<\/p>(.+?)<p>{\/details}<\/p>/gms, '<details class="border mb-3"><summary class="bg-light p-3">$1</summary><div class="border-top p-3 bg-white">$2</div></details>')
             + '</div>'
         ;
         const titleMatch = content.match('<h1[^>]*>([^<]+)</h1>');
