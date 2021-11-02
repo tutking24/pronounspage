91e5aafaf575c5a2ab0248fd35882b162de9f678
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-11-02 14:12:47 +0000

    [fr] Update events.js

--- locale/fr/events.js
+++ locale/fr/events.js
@@ -2,7 +2,7 @@ const {Event, day, week, month, dayYear, EventLevel} = require("../../src/calend
 
 module.exports = [
     // months
-    new Event('Mois de l''histoire des LGBT (Canada/USA/Australia)', 'Progress Pride', 10, month, EventLevel.Month),
+    new Event('Mois de l\'histoire des LGBT (Canada/USA/Australia)', 'Progress Pride', 10, month, EventLevel.Month),
 
     // static date
     new Event('Journée du triangle rose (Canada)', null, 2, day(14), EventLevel.Day),
@@ -12,9 +12,10 @@ module.exports = [
     new Event('Journée de sensibilisation au VIH/sida des communautés noires (Canada/USA)', null, 2, day(7), EventLevel.Day),
     new Event('Journée de sensibilité à la bispiritualité (Canada/USA)', 'Two Spirit', 7, day(11), EventLevel.Day, ['two spirit']),
     new Event('Journée de sensibilisation aux femmes, aux filles et aux personnes bispirituelles autochtones disparues et assassinées (Canada/USA)', 'Two Spirit', 5, day(5), EventLevel.Day, ['two spirit']),
+    new Event('Journée du dépistage du VIH (Canada/USA)', null, 6, day(27), EventLevel.Day),
 
     // dynamic date
-    new Event('Journée d''esprit (Canada/USA)', null, 10, function* (monthDays) {
+    new Event('Journée d\'esprit (Canada/USA)', null, 10, function* (monthDays) {
         let thursdays = 0;
         for (let d of monthDays) {
             if (d.dayOfWeek === 4) {
