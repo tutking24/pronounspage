f882a67d9a0bc1eacf44af1c2ecacf49c779f83e
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-11-02 22:26:15 +0000

    [fr] Update events.js

--- locale/fr/events.js
+++ locale/fr/events.js
@@ -14,6 +14,7 @@ module.exports = [
     new Event('Journée de sensibilisation aux femmes, aux filles et aux personnes bispirituelles autochtones disparues et assassinées (Canada/USA)', 'Two Spirit', 5, day(5), EventLevel.Day, ['two spirit']),
     new Event('Journée du dépistage du VIH (Canada/USA)', null, 6, day(27), EventLevel.Day),
     new Event('Journée de la sensibilisation du VIH/sida et au vieillissement (Canada/USA)', null, 9, day(18), EventLevel.Day),
+    new Event('Anniversaire de la décrimilisation de l\'homosexualité (Canada)', null, 6, day(27), EventLevel.Day),
 
     // dynamic date
     new Event('Journée d\'esprit (Canada/USA)', null, 10, function* (monthDays) {
