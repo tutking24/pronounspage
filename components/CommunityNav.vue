8d8a239d4aea17f823db8d04e7b4c4e1c5ecb54c
Avris <andre@avris.it>
2021-11-10 13:05:08 +0100

    [team] add link in <CommunityNav/>

--- components/CommunityNav.vue
+++ components/CommunityNav.vue
@@ -5,5 +5,18 @@
         {name: 'calendar.header', route: config.calendar.route, icon: 'calendar-star', condition: config.calendar && config.calendar.enabled},
         {name: 'census.header', route: config.census.route, icon: 'user-chart', condition: config.census.enabled},
         {name: 'people.header', route: config.people.route, icon: 'user-friends', condition: config.people.enabled},
+        {name: 'contact.team.nameShort', route: config.contact.team.route, icon: 'collective-logo.svg', iconInverse: darkMode},
     ]"/>
 </template>
+
+<script>
+import {mapState} from "vuex";
+
+export default {
+    computed: {
+        ...mapState([
+            'darkMode',
+        ]),
+    },
+};
+</script>
