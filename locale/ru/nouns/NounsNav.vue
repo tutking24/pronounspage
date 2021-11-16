f207920750e37f120e1027b722deea6837963665
Andrea Vos <andrea@avris.it>
2021-11-16 18:16:06 +0100

    [ru] update pronouns, nouns

--- locale/ru/nouns/NounsNav.vue
+++ locale/ru/nouns/NounsNav.vue
@@ -1,8 +1,2 @@
 <template>
-    <Submenu :links="[
-        {name: 'nouns.neuterNouns.header', icon: 'deer', route: 'neutratywy'},
-        {name: 'nouns.dukajNouns.header', icon: 'ghost', route: 'dukatywy'},
-        {name: 'nouns.personNouns.header', icon: 'user-friends', route: 'osobatywy'},
-        {name: 'nouns.xNouns.header', icon: 'comment-times', route: 'iksatywy'},
-    ]"/>
 </template>
