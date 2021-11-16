0c2bdacd39232aca676796f1e12abffec63e91c2
Avris <andre@avris.it>
2021-11-16 18:09:09 +0100

    [ru] update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -183,79 +183,79 @@ export default {
     router: {
         extendRoutes(routes, resolve) {
             if (config.pronouns.enabled) {
-                routes.push({ path: '/' + config.pronouns.route, component: resolve(__dirname, 'routes/pronouns.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.pronouns.route), component: resolve(__dirname, 'routes/pronouns.vue') });
             }
 
             if (config.sources.enabled) {
-                routes.push({ path: '/' + config.sources.route, component: resolve(__dirname, 'routes/sources.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.sources.route), component: resolve(__dirname, 'routes/sources.vue') });
             }
 
             if (config.nouns.enabled) {
-                routes.push({ path: '/' + config.nouns.route, component: resolve(__dirname, 'routes/nouns.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.nouns.route), component: resolve(__dirname, 'routes/nouns.vue') });
                 for (let subroute of config.nouns.subroutes || []) {
-                    routes.push({ path: `/${subroute}`, component: resolve(__dirname, `data/nouns/${subroute}.vue`) });
+                    routes.push({ path: '/' + encodeURIComponent(subroute), component: resolve(__dirname, `data/nouns/${subroute}.vue`) });
                 }
             }
 
             if (config.inclusive.enabled) {
-                routes.push({path: `/${config.inclusive.route}`, component: resolve(__dirname, 'routes/inclusive.vue')});
+                routes.push({path: '/' + encodeURIComponent(config.inclusive.route), component: resolve(__dirname, 'routes/inclusive.vue')});
             }
             if (config.terminology.enabled) {
-                routes.push({path: `/${config.terminology.route}`, component: resolve(__dirname, 'routes/terminology.vue')});
+                routes.push({path: '/' + encodeURIComponent(config.terminology.route), component: resolve(__dirname, 'routes/terminology.vue')});
 
                 // TODO remove later
-                routes.push({path: `/${config.nouns.route}/${config.terminology.route}`, component: resolve(__dirname, 'routes/terminology.vue')});
+                routes.push({path: '/' + encodeURIComponent(config.nouns.route) + '/' + encodeURIComponent(config.terminology.route), component: resolve(__dirname, 'routes/terminology.vue')});
             }
 
             if (config.names.enabled) {
-                routes.push({ path: '/' + config.names.route, component: resolve(__dirname, 'routes/names.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.names.route), component: resolve(__dirname, 'routes/names.vue') });
             }
 
             if (config.faq.enabled) {
-                routes.push({ path: '/' + config.faq.route, component: resolve(__dirname, 'routes/faq.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.faq.route), component: resolve(__dirname, 'routes/faq.vue') });
             }
 
             if (config.links.enabled) {
-                routes.push({ path: '/' + config.links.route, component: resolve(__dirname, 'routes/links.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.links.route), component: resolve(__dirname, 'routes/links.vue') });
                 if (config.links.academicRoute) {
-                    routes.push({ path: '/' + config.links.academicRoute, component: resolve(__dirname, 'routes/academic.vue') });
+                    routes.push({ path: '/' + encodeURIComponent(config.links.academicRoute), component: resolve(__dirname, 'routes/academic.vue') });
                 }
                 if (config.links.mediaRoute) {
-                    routes.push({ path: '/' + config.links.mediaRoute, component: resolve(__dirname, 'routes/media.vue') });
+                    routes.push({ path: '/' + encodeURIComponent(config.links.mediaRoute), component: resolve(__dirname, 'routes/media.vue') });
                 }
             }
 
             if (config.links.blog) {
-                routes.push({ path: '/' + config.links.blogRoute, component: resolve(__dirname, 'routes/blog.vue'), name: 'blog' });
+                routes.push({ path: '/' + encodeURIComponent(config.links.blogRoute), component: resolve(__dirname, 'routes/blog.vue'), name: 'blog' });
             }
-            routes.push({ path: '/' + config.links.blogRoute + '/:slug', component: resolve(__dirname, 'routes/blogEntry.vue'), name: 'blogEntry' });
+            routes.push({ path: '/' + encodeURIComponent(config.links.blogRoute) + '/:slug', component: resolve(__dirname, 'routes/blogEntry.vue'), name: 'blogEntry' });
 
             if (config.links.zine && config.links.zine.enabled) {
-                routes.push({ path: '/' + config.links.zine.route, component: resolve(__dirname, 'routes/zine.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.links.zine.route), component: resolve(__dirname, 'routes/zine.vue') });
             }
 
             if (config.people.enabled) {
-                routes.push({ path: '/' + config.people.route, component: resolve(__dirname, 'routes/people.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.people.route), component: resolve(__dirname, 'routes/people.vue') });
             }
 
             if (config.english.enabled) {
-                routes.push({ path: '/' + config.english.route, component: resolve(__dirname, 'routes/english.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.english.route), component: resolve(__dirname, 'routes/english.vue') });
             }
 
             if (config.contact.enabled) {
-                routes.push({ path: '/' + config.contact.route, component: resolve(__dirname, 'routes/contact.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.contact.route), component: resolve(__dirname, 'routes/contact.vue') });
             }
             if (config.contact.team.enabled) {
-                routes.push({ path: '/' + config.contact.team.route, component: resolve(__dirname, 'routes/team.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.contact.team.route), component: resolve(__dirname, 'routes/team.vue') });
             }
 
             if (config.census.enabled) {
-                routes.push({ path: '/' + config.census.route, component: resolve(__dirname, 'routes/census.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.census.route), component: resolve(__dirname, 'routes/census.vue') });
             }
 
             if (config.user.enabled) {
-                routes.push({path: '/' + config.user.route, component: resolve(__dirname, 'routes/user.vue')});
-                routes.push({path: '/' + config.user.termsRoute, component: resolve(__dirname, 'routes/terms.vue')});
+                routes.push({path: '/' + encodeURIComponent(config.user.route), component: resolve(__dirname, 'routes/user.vue')});
+                routes.push({path: '/' + encodeURIComponent(config.user.termsRoute), component: resolve(__dirname, 'routes/terms.vue')});
             }
             routes.push({ path: '/license', component: resolve(__dirname, 'routes/license.vue') });
             routes.push({ path: '/admin', component: resolve(__dirname, 'routes/admin.vue') });
@@ -269,18 +269,18 @@ export default {
             }
 
             if (config.pronouns.enabled) {
-                routes.push({ path: '/' + config.pronouns.any, component: resolve(__dirname, 'routes/any.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.pronouns.any), component: resolve(__dirname, 'routes/any.vue') });
                 if (config.pronouns.null && config.pronouns.null.routes) {
                     for (let route of config.pronouns.null.routes) {
-                        routes.push({ path: '/' + route, component: resolve(__dirname, 'routes/avoiding.vue') });
+                        routes.push({ path: '/' + encodeURIComponent(route), component: resolve(__dirname, 'routes/avoiding.vue') });
                     }
                 }
             }
 
             if (config.calendar && config.calendar.enabled) {
-                routes.push({ path: '/' + config.calendar.route, component: resolve(__dirname, 'routes/calendar.vue') });
-                routes.push({ path: '/' + config.calendar.route + '/:year(\\d\\d\\d\\d)', component: resolve(__dirname, 'routes/calendar.vue') });
-                routes.push({ path: '/' + config.calendar.route + '/:year(\\d\\d\\d\\d)-:month(\\d\\d)-:day(\\d\\d)', component: resolve(__dirname, 'routes/calendarDay.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.calendar.route), component: resolve(__dirname, 'routes/calendar.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.calendar.route) + '/:year(\\d\\d\\d\\d)', component: resolve(__dirname, 'routes/calendar.vue') });
+                routes.push({ path: '/' + encodeURIComponent(config.calendar.route) + '/:year(\\d\\d\\d\\d)-:month(\\d\\d)-:day(\\d\\d)', component: resolve(__dirname, 'routes/calendarDay.vue') });
             }
 
             if (config.api !== null) {
