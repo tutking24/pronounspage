665c47d9649310675367dc2fe1336fc61ec335f5
Avris <andre@avris.it>
2021-11-16 17:21:03 +0100

    [ru] init (based on pl)

---
+++ locale/ru/nouns/NounsNav.vue
@@ -0,0 +1,8 @@
+<template>
+    <Submenu :links="[
+        {name: 'nouns.neuterNouns.header', icon: 'deer', route: 'neutratywy'},
+        {name: 'nouns.dukajNouns.header', icon: 'ghost', route: 'dukatywy'},
+        {name: 'nouns.personNouns.header', icon: 'user-friends', route: 'osobatywy'},
+        {name: 'nouns.xNouns.header', icon: 'comment-times', route: 'iksatywy'},
+    ]"/>
+</template>
