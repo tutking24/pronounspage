18aea9114838ffedc45ddf8d1e75de234ee2f7fa
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-11-17 01:21:46 +0000

    [ru] Update events.js

--- locale/ru/events.js
+++ locale/ru/events.js
@@ -1,4 +1,7 @@
 const {Event, day, week, month, dayYear, EventLevel} = require("../../src/calendar/helpers");
 
 module.exports = [
+    // static date
+    new Event('Годовщина декриминализации гомосексуальности в России (1993)', null, 4, day(29), EventLevel.Day),
+    new Event('Годовщина декриминализации гомосексуальности в беларуси (1994)', null, 3, day(1), EventLevel.Day),
 ];
