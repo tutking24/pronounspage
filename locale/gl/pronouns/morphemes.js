846ddfeea01420d4ec5ea8f297f0d3685edc2456
Avris <andre@avris.it>
2021-11-26 13:36:37 +0100

    [gl] init

---
+++ locale/gl/pronouns/morphemes.js
@@ -0,0 +1,13 @@
+export default [
+    'definite_article',
+    'pronoun',
+    'inflection',
+    'de_pronoun',
+    'indefinite_article',
+    'plural_pronoun',
+    'de_plural',
+    'plural_definite_article',
+    'plural_indefinite_article',
+    'inflection_c',
+    'possessive',
+];
