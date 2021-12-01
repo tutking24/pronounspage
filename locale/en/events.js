2bc32bfe166b8ebf47772f07b52ee583dd841884
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-12-01 06:03:18 +0000

    [en] Update events.js

--- locale/en/events.js
+++ locale/en/events.js
@@ -53,7 +53,6 @@ module.exports = [
     new Event('{https://en.wikipedia.org/wiki/Matthew_Shepard=Matthew Shepard} Day of Remembrance', null, 10, day(12), EventLevel.Day),
     new Event('{/terminology#transgender=Trans-}, {/terminology#travesti=Travesti} and {{/terminology#nonbinary=Non-Binary} Visibility Day (Paraguay)', 'Transgender', 10, day(15), EventLevel.Day, ['transgender', 'nonbinary', 'travesti']),
     new Event('Missing and Murdered Indigenous Women, Girls, and {/terminology#two%20spirit=Two Spirit} Awareness Day', 'Two Spirit', 5, day(5), EventLevel.Day, ['two spirit']),
-    new Event('Gender Expansive Parents\' Day (US)', null, 12, day(6), EventLevel.Day),
     new Event('{/terminology#travesti=Travesti} and {/terminology#transgender=Transgender} Pride Day (Brazil)', 'Transgender', 5, day(15), EventLevel.Day, ['transgender', 'travesti']),
 
     // dynamic date
