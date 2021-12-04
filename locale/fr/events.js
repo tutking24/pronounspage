5f4aa8d62cbdbf5022bc5f5f04a84e98475cbd8c
Paweł Dembowski <pawel.dembowski@gmail.com>
2021-12-04 00:10:01 +0000

    [fr] Update events.js

--- locale/fr/events.js
+++ locale/fr/events.js
@@ -5,10 +5,10 @@ module.exports = [
     new Event('Mois de l\'histoire des LGBT (Canada/USA/Australia)', 'Progress Pride', 10, month, EventLevel.Month),
 
     // static date
-    new Event('Journée du triangle rose (Canada)', null, 2, day(14), EventLevel.Day),
-    new Event('Journée de le mariage pour tous (Canada)', null, 7, day(20), EventLevel.Day),
-    new Event('Journée de le mariage pour tous (France)', null, 5, day(18), EventLevel.Day),
-    new Event('Journée de le mariage pour tous (Belgique)', null, 6, day(1), EventLevel.Day),
+    new Event('Journée du triangle rose (Canada)', '_hrc', 2, day(14), EventLevel.Day),
+    new Event('Journée de le mariage pour tous (Canada)', '_hrc', 7, day(20), EventLevel.Day),
+    new Event('Journée de le mariage pour tous (France)', '_hrc', 5, day(18), EventLevel.Day),
+    new Event('Journée de le mariage pour tous (Belgique)', '_hrc', 6, day(1), EventLevel.Day),
     new Event('Journée de sensibilisation au VIH/sida des communautés noires (Canada/USA)', '_red-ribbon', 2, day(7), EventLevel.Day, ['aids']),
     new Event('Journée de sensibilité à la bispiritualité', 'Two Spirit', 7, day(11), EventLevel.Day, ['two spirit']),
     new Event('Journée de sensibilisation aux femmes, aux filles et aux personnes bispirituelles autochtones disparues et assassinées (Canada/USA)', 'Two Spirit', 5, day(5), EventLevel.Day, ['two spirit']),
