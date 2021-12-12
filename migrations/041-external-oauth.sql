d097208f2966ffa7cc0145f11e93ce787850c796
Lauren Liberda <laura@selfisekai.rocks>
2021-12-12 23:59:26 +0000

    more universal oauth key storage

--- migrations/041-external-oauth.sql
+++ migrations/041-external-oauth.sql
@@ -1,11 +1,12 @@
 -- Up
 
-CREATE TABLE mastodon_oauth (
+CREATE TABLE oauth_keys (
     instance TEXT NOT NULL PRIMARY KEY,
+    provider TEXT NOT NULL,
     client_id TEXT NOT NULL,
     client_secret TEXT NOT NULL
 );
 
 -- Down
 
-DROP TABLE mastodon_oauth;
+DROP TABLE oauth_keys;
