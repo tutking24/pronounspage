663579e4f3cc3b8c80ac4dae96cdb4895c439e17
Andrea Vos <andrea@avris.it>
2021-12-17 00:16:05 +0100

    [pronouns][bug] fix missing config in api

--- server/routes/pronouns.js
+++ server/routes/pronouns.js
@@ -61,7 +61,7 @@ router.get('/pronouns/:pronoun*', handleErrorAsync(async (req, res) => {
 router.get('/pronouns-name/:pronoun*', handleErrorAsync(async (req, res) => {
     const pronoun = buildPronoun(
         parsePronouns(loadTsv('pronouns/pronouns')),
-        req.params.pronoun + req.params[0],
+        decodeURIComponent(req.params.pronoun + req.params[0]),
     );
     if (!pronoun) {
         return res.status(404).json({error: 'Not found'});
