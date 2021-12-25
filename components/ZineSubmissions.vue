0879623fe486fefc318f7582742bef32a91e816a
Andrea Vos <andrea@avris.it>
2021-12-26 00:55:10 +0100

    [pl][zine] prepare for closing submissions

---
+++ components/ZineSubmissions.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul class="list-unstyled">
+            <li v-for="{icon, label, content} in $t('links.zine.submissions.rules')" class="mb-2">
+                <h4>
+                    <Icon :v="icon"/>
+                    <LinkedText :text="label"/>:
+                </h4>
+                <ul v-if="Array.isArray(content)">
+                    <li v-for="el in content"><LinkedText :text="el"/></li>
+                </ul>
+                <p v-else>
+                    <LinkedText :text="content"/>
+                </p>
+            </li>
+        </ul>
+
+        <p class="small">
+            <Icon v="info-circle"/>
+            <T>links.zine.submissions.disclaimer</T>
+        </p>
+    </div>
+</template>
--- routes/zine.vue
+++ routes/zine.vue
@@ -9,31 +9,25 @@
 
         <T>links.zine.info</T>
 
-        <h3>
+        <h3 class="mb-3">
             <Icon v="bullhorn"/>
             <T>links.zine.submissions.header</T>
         </h3>
 
-        <div class="alert alert-info">
-            <ul class="list-unstyled">
-                <li v-for="{icon, label, content} in $t('links.zine.submissions.rules')" class="mb-2">
-                    <h4>
-                        <Icon :v="icon"/>
-                        <LinkedText :text="label"/>:
-                    </h4>
-                    <ul v-if="Array.isArray(content)">
-                        <li v-for="el in content"><LinkedText :text="el"/></li>
-                    </ul>
-                    <p v-else>
-                        <LinkedText :text="content"/>
-                    </p>
-                </li>
-            </ul>
-
-            <p class="small">
-                <Icon v="info-circle"/>
-                <T>links.zine.submissions.disclaimer</T>
-            </p>
+        <ZineSubmissions v-if="config.links.zine.open" class="alert alert-info"/>
+        <div v-else>
+            <div class="alert alert-warning">
+                <p class="h6 mb-0">
+                    <Icon v="exclamation-triangle"/>
+                    <T>links.zine.submissions.closed</T>
+                </p>
+            </div>
+            <details class="border mb-3">
+                <summary class="bg-light p-3">
+                    <T>links.zine.submissions.header</T>
+                </summary>
+                <ZineSubmissions class="p-3 border-top"/>
+            </details>
         </div>
 
         <Separator icon="heart"/>
