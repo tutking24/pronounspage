ad3b239ba997376bf3335358c60fcd6d382f68dc
Andrea <andrea@avris.it>
2021-12-28 11:11:48 +0100

    [lad] init

--- src/locales.js
+++ src/locales.js
@@ -4,6 +4,7 @@ module.exports = [
     ['en', 'English',        'https://en.pronouns.page', true],
     ['fr', 'Français',       'https://fr.pronouns.page', true],
     ['gl', 'Galego',         'https://gl.pronouns.page', false],
+    ['lad', 'Ladino',        'https://lad.pronouns.page', false],
     ['nl', 'Nederlands',     'https://nl.pronouns.page', true],
     ['no', 'Norsk (Bokmål)', 'https://no.pronouns.page', true],
     ['pl', 'Polski',         'https://zaimki.pl',        true],
