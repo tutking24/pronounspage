bebdba932d9a30574e0549e465941be3b550f585
Andrea Vos <andrea@avris.it>
2021-12-29 18:11:26 +0100

    [links] sibling projects

---
+++ components/Siblings.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul class="list-unstyled mb-4">
+        <li class="mb-2">
+            <a href="https://cake.avris.it" target="_blank" rel="noopener">
+                <Icon v="cake.png"/>
+                Attraction Layer Cake
+            </a>
+        </li>
+        <li class="mb-2">
+            <a href="https://spectrum.avris.it" target="_blank" rel="noopener">
+                <Icon v="spectrum.png"/>
+                Gender & Sexuality Spectrum
+            </a>
+        </li>
+        <li class="mb-2">
+            <a href="https://emojiflags.avris.it/" target="_blank" rel="noopener">
+                <Icon v="emojiflags.png"/>
+                Emoji Pride Flags
+            </a>
+        </li>
+    </ul>
+</template>
