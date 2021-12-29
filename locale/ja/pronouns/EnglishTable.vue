3d1d6865965358741b9943a3f709cbf68bda0e7b
Andrea Vos <andrea@avris.it>
2021-12-29 19:26:51 +0100

    [ja][english]

--- locale/ja/pronouns/EnglishTable.vue
+++ locale/ja/pronouns/EnglishTable.vue
@@ -1,3 +1,42 @@
 <template>
-    <div></div>
+    <div class="table-responsive">
+        <table class="table text-nowrap">
+            <thead>
+            <tr>
+                <th>Japanese</th>
+                <th v-if="t[0].formal !== undefined">Formal?</th>
+                <th>Romanised</th>
+                <th>IPA pronunciation</th>
+                <th>Meaning</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="{pronoun, formal, romanised, ipa, meaning} in t">
+                <th>
+                    <LinkedText :text="pronoun"/>
+                </th>
+                <td v-if="formal !== undefined">
+                    {{ formal ? 'Yes' : 'No' }}
+                </td>
+                <td>
+                    {{ romanised }}
+                </td>
+                <td>
+                    {{ ipa }}
+                </td>
+                <td>
+                    {{ meaning }}
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
+
+<script>
+    export default {
+        props: {
+            t: { required: true },
+        },
+    }
+</script>
