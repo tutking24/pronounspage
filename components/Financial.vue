d53d927724551734a42ac5644e99d7c1162c0548
Andrea Vos <andrea@avris.it>
2021-12-30 20:15:18 +0100

    [footer][support] link to financial transparency in the footer and support widget

---
+++ components/Financial.vue
@@ -0,0 +1,6 @@
+<template>
+    <span>
+        <T>footer.financial</T><T>quotation.colon</T>
+        <LocaleLink locale="en" link="/blog/financial-transparency-2020-2021">2020-2021</LocaleLink>
+    </span>
+</template>
