0c97aa991cea648c5ae895115f375d743e35c25f
Andrea <andrea@avris.it>
2022-01-01 16:17:31 +0100

    [pl][census] updates

--- src/helpers.js
+++ src/helpers.js
@@ -169,7 +169,8 @@ export const zip = (list, reverse) => {
 }
 
 // https://stackoverflow.com/a/6274381/3297012
-export const shuffle = a => {
+export const shuffle = array => {
+    const a = [...array];
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [a[i], a[j]] = [a[j], a[i]];
