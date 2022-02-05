3abad0bbfe0d27a6680a2104d381bee44a99b702
Andrea Vos <andrea@avris.it>
2022-02-05 21:50:58 +0100

    [pl][census] banner

--- routes/census.vue
+++ routes/census.vue
@@ -272,6 +272,7 @@
             return head({
                 title: this.$t('census.headerLong'),
                 description: this.$t('census.description')[0],
+                banner: 'img-local/census-banner.png',
             });
         },
     };
