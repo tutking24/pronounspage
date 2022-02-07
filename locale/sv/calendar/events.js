3a04bb48f792a88266028822272ad1a6578eb85a
Paweł Dembowski <pawel.dembowski@gmail.com>
2022-02-07 22:55:33 +0000

    [sv] Update events.js

--- locale/sv/calendar/events.js
+++ locale/sv/calendar/events.js
@@ -1,4 +1,9 @@
-const {Event, day, week, month, dayYear, EventLevel} = require("../../../src/calendar/helpers");
+const {Event, day, week, month, dayYear, EventLevel, Day} = require("../../../src/calendar/helpers");
 
 module.exports = [
+
+    // --- static date ---
+    new Event('Marriage Equality Day (Sweden)', '_hrc', 5, day(1), EventLevel.Day),
+    new Event('Marriage Equality Day (Sweden)', '_hrc', 3, day(1), EventLevel.Day),
+
 ];
