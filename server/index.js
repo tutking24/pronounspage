43498b5bc590887d5eeda593266e1ab05a37b508
Andrea <andrea@avris.it>
2022-02-12 14:21:40 +0100

    [perf] sqlite busy timeout

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ class LazyDatabase {
         if (this.db === null) {
             this.db = await dbConnection();
             // await this.db.get('PRAGMA journal_mode = WAL;');
-            // await this.db.get('PRAGMA busy_timeout = 5000;');
+            await this.db.get('PRAGMA busy_timeout = 5000;');
             // await this.db.get('PRAGMA foreign_keys = ON;')
         }
     }
