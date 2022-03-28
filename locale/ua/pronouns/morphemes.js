4e7772e0ab9c3497e37bb7aa841b877a844292bc
Andrea <andrea@avris.it>
2022-03-28 19:55:41 +0200

    [ua] base

---
+++ locale/ua/pronouns/morphemes.js
@@ -0,0 +1,16 @@
+export default [
+    'nominative',
+    'genitive',
+    'nonreflexive_verb_past',
+    'dative',
+    'accusative',
+    'instrumental',
+    'prepositional',
+    'instrumental_with_preposition',
+    'genitive_with_preposition',
+    'reflexive_verb_past',
+    'short_adjective',
+    'adjective',
+    'possessive',
+    'definitive',
+];
