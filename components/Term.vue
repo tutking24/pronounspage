0e699414361bbc5c6235cce1257c70a7cb4f1f6b
Andrea <andrea@avris.it>
2022-03-31 23:35:09 +0200

    #269 reduce S3 usage - smaller avatars, terminology flags

--- components/Term.vue
+++ components/Term.vue
@@ -26,7 +26,7 @@
 
         <p v-if="flags && (term.flags.length || term.images.length)" class="text-center">
             <img v-for="flag in term.flags" :src="`/flags/${flag}.png`" class="flag m-1" loading="lazy"/>
-            <img v-for="image in term.images" :src="buildImageUrl(image, 'big')" class="flag m-1" loading="lazy"/>
+            <img v-for="image in term.images" :src="buildImageUrl(image, 'flag')" class="flag m-1" loading="lazy"/>
         </p>
 
         <div v-if="config.calendar.enabled && events && events.length" class="alert">
