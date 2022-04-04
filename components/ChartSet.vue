c99e2acd3860d8bd32ba8f42ea256e5338b52870
Andrea Vos <andrea@avris.it>
2022-04-04 22:50:39 +0200

    typo

--- components/ChartSet.vue
+++ components/ChartSet.vue
@@ -10,8 +10,8 @@
             </div>
         </div>
         <div class="card-body" v-if="mode">
-            <Chart :data="data" :label="`cumulative ${name}`" v-show="mode === 'daily'"/>
-            <Chart :data="data" :label="`new ${name} per day`" cumulative v-show="mode === 'cumulative'"/>
+            <Chart :data="data" :label="`new ${name} per day`" v-show="mode === 'daily'"/>
+            <Chart :data="data" :label="`cumulative ${name}`" cumulative v-show="mode === 'cumulative'"/>
         </div>
     </div>
 </template>
