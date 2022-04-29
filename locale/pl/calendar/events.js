74dc4299c69c7e9e750e6c8a0706ada9b29dface
Paweł Dembowski <pawel.dembowski@gmail.com>
2022-04-29 21:30:51 +0000

    [pl] Update events.js

--- locale/pl/calendar/events.js
+++ locale/pl/calendar/events.js
@@ -62,6 +62,7 @@ module.exports = [
     new Event('{https://www.facebook.com/events/1593099004379743=IV Marsz Równości w Opolu}', 'LGBTQ', 7, dayYear(16, 2022), EventLevel.Day),
     new Event('{https://www.facebook.com/events/698107478207024=3. Marsz Równości na Woodstocku} (Czaplinek)', 'LGBTQ', 8, dayYear(6, 2022), EventLevel.Day),
     new Event('{https://www.facebook.com/events/521102249311233=Szczeciński Marsz Równości}', 'LGBTQ', 8, dayYear(20, 2022), EventLevel.Day),
+    new Event('{https://www.facebook.com/events/413636720098790/=I Marsz Równości w Miliczu}', 'LGBTQ', 5, dayYear(22, 2022), EventLevel.Day),
 
     // --- dynamic date ---
 
