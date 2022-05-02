a8a2fe204f541970cbdb7fc5a8753531c79b8b88
Paweł Dembowski <pawel.dembowski@gmail.com>
2022-05-02 23:08:23 +0000

    [sv] Update events.js

--- locale/sv/calendar/events.js
+++ locale/sv/calendar/events.js
@@ -1,6 +1,9 @@
 const {Event, day, week, month, dayYear, EventLevel, Day} = require("../../../src/calendar/helpers");
 
 module.exports = [
+    // --- months ---
+    new Event('Queer History Month (Finland)', 'Progress Pride', 11, month, EventLevel.Month),    
+
 
     // --- static date ---
     new Event('Marriage Equality Day (Sweden)', '_hrc', 5, day(1), EventLevel.Day),
