6f60cf1bd2e42bbfa760f89626ef9517b89a1fc4
Andrea Vos <andrea@avris.it>
2022-05-07 15:16:44 -0500

    ukraine solidarity banner - fixes

--- components/UkraineBanner.vue
+++ components/UkraineBanner.vue
@@ -6,18 +6,26 @@
     </div>
 </template>
 
-
 <style lang="scss">
 @import "assets/variables";
 
+$ukraine-blue: #025abc;
+$ukraine-golden: #fed602;
+
 .ukraine-banner {
-    background-color: #025abc;
-    background: linear-gradient(90deg, #025abc 40%, #fed602 60%);
-    color: white;
+    background-color: $ukraine-blue;
+    background: linear-gradient(90deg, $ukraine-blue 40%, $ukraine-golden 60%);
+    color: $white;
+    text-shadow: 2px 2px 2px rgba($black, .5);
 
     a {
         color: $white;
         text-decoration: underline;
     }
+}
+
+body[data-theme="dark"] .ukraine-banner a {
+    color: $white;
+    text-decoration: underline;
 }
 </style>
