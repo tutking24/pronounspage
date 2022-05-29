041b7d017c85de689d9def66ebee6ac05b282a21
Paweł Dembowski <pawel.dembowski@gmail.com>
2022-05-29 17:31:42 +0000

    [pl] Update events.js

--- locale/en/calendar/events.js
+++ locale/en/calendar/events.js
@@ -195,6 +195,8 @@ module.exports = [
     new Event('Day of Silence', null, 4, dayYear(23, 2021), EventLevel.Day),
     new Event('Day of Silence', null, 4, dayYear(22, 2022), EventLevel.Day),
     new Event('LGBTQIA+ Equal Pay Awareness Day', '_hrc', 6, dayYear(16, 2021), EventLevel.Day),
+    new Event('Spring European HIV Testing Week', '_red-ribbon', 5, weekStarting(16, 2022), EventLevel.Week, ['aids']),
+    new Event('Autumn European HIV Testing Week', '_red-ribbon', 11, weekStarting(21, 2022), EventLevel.Week, ['aids']),    
 
     // --- one-off events ---
     new Event('{https://www.darknessintolight.ie/=Darkness into Light} (Ireland)', '_yellow-ribbon', 5, dayYear(7, 2022), EventLevel.Day),
