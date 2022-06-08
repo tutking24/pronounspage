b6c0c8c074f799cac78b3600e9fa0c3cb8850823
Andrea <andrea@avris.it>
2022-06-08 09:22:01 +0200

    reenable twemoji

--- components/Twemoji.vue
+++ components/Twemoji.vue
@@ -1,40 +1,34 @@
 <template>
-    <!-- twemoji disabled, testing Noto Emoji instead -->
-    <span>
-        <slot ref="source"></slot>
-    </span>
-    <!--
     <span>
         <span ref="source" v-show="false">
             <slot ref="source"></slot>
         </span>
         <span ref="target"></span>
     </span>
-    -->
 </template>
 
 <script>
-// import twemoji from 'twemoji';
+import twemoji from 'twemoji';
 
 export default {
-    // mounted() {
-    //     this.update();
-    //
-    //     const observer = new MutationObserver(this.update);
-    //     observer.observe(this.$refs.source, {
-    //         childList: true,
-    //         subtree: true
-    //     });
-    //     this.observer = observer;
-    // },
-    // beforeUnmount() {
-    //     this.observer.disconnect();
-    // },
-    // methods: {
-    //     update() {
-    //         this.$refs.target.innerHTML = twemoji.parse(this.$refs.source.innerHTML);
-    //     },
-    // },
+    mounted() {
+        this.update();
+
+        const observer = new MutationObserver(this.update);
+        observer.observe(this.$refs.source, {
+            childList: true,
+            subtree: true
+        });
+        this.observer = observer;
+    },
+    beforeUnmount() {
+        this.observer.disconnect();
+    },
+    methods: {
+        update() {
+            this.$refs.target.innerHTML = twemoji.parse(this.$refs.source.innerHTML);
+        },
+    },
 };
 </script>
 
