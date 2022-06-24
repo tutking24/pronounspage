e88130cf974c4497dd9cc205f4d02c352e6f19c3
Andrea Vos <andrea@avris.it>
2022-06-24 08:36:55 +0200

    [tok] cartouches

--- components/Profile.vue
+++ components/Profile.vue
@@ -62,7 +62,7 @@
                 </h3>
 
                 <ul class="list-unstyled">
-                    <li v-for="(opinion, name) in profile.names"><Opinion :word="convertName(name)" :opinion="opinion"/></li>
+                    <li v-for="(opinion, name) in profile.names"><Opinion :word="convertName(name)" :opinion="opinion" :escape="false"/></li>
                 </ul>
             </div>
             <div v-if="Object.keys(profile.pronouns).length" :class="['col-6', mainRowCount === 3 ? 'col-lg-4' : 'col-lg-6']">
