7b3e1fb9e437e3f509eb74047fe31ab6dd135aa7
Paweł Dembowski <pawel.dembowski@gmail.com>
2022-06-24 20:32:29 +0000

    [en] Update events.js

--- locale/en/calendar/events.js
+++ locale/en/calendar/events.js
@@ -92,7 +92,7 @@ module.exports = [
     new Event('{/terminology#lesbian=Lesbian} {https://www.bng.gal/articulo/novas/dia-visibilidade-lesbica-galega/20210608103130030074.html=Visibility Day} (Galicia)', 'Lesbian', 6, day(8), EventLevel.Day, ['lesbian']),
     new Event('{https://napwha.org.au/about-us/national-network-of-women/=National Day of Women living with HIV} (Australia)', '_red-ribbon', 3, day(9), EventLevel.Day, ['aids']),
     new Event('{/terminology#nonbinary=Nonbinary} Kids\' Day', 'Nonbinary', 10, day(1), EventLevel.Day, ['nonbinary']),
-    new Event('{https://en.wikipedia.org/wiki/David_Kato=Kuchu Remembrance Day}', '_black-ribbon', 1, day(26), EventLevel.Day),
+    new Event('{https://en.wikipedia.org/wiki/David_Kato=Kuchu Remembrance Day} (Uganda)', '_black-ribbon', 1, day(26), EventLevel.Day),
 
     // --- dynamic date ---
 
