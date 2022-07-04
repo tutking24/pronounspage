5005b1bae5d2dbd60274bf94b88bd11ffd9c4b75
Paweł Dembowski <pawel.dembowski@gmail.com>
2022-07-04 23:36:39 +0000

    [pl] Update events.js

--- locale/pl/calendar/events.js
+++ locale/pl/calendar/events.js
@@ -9,6 +9,7 @@ module.exports = [
     new Event('Miesiąc Historii LGBTQ+ (Włochy)', 'Progress Pride', 4, month, EventLevel.Month),
     new Event('Miesiąc Historii LGBTQ+ (Finlandia)', 'Progress Pride', 11, month, EventLevel.Month),
     new Event('Miesiąc Historii Osób {/terminology#transgender=Transpłciowych} (USA)', 'Transgender', 8, month, EventLevel.Month, ['transgender']),
+    new Event('Miesiąc Gniewu', 'Progress Pride', 7, month, EventLevel.Month),    
 
     // --- static date ---
     new Event('Tęczowa Noc', null, 8, day(7), EventLevel.Day),
