d67dfb5c50ba01f639d8eaee82b00515b8cf6130
Andrea <andrea@avris.it>
2022-07-23 13:49:12 +0200

    [tok] "ona" disclaimer, revert "just ona" (routing conflict)

--- locale/tok/pronouns/Suggested.vue
+++ locale/tok/pronouns/Suggested.vue
@@ -1,6 +1,13 @@
 <template>
-    <h2>
-        <Icon v="tags"/>
-        <T>home.pronouns</T>
-    </h2>
+    <section>
+        <h2>
+            <Icon v="tags"/>
+            <T>home.pronouns</T>
+        </h2>
+        <p>
+            Toki Pona uses just one 3rd person personal pronoun, “ona”, which is not gendered.
+            Gender <em>may</em> be specified in rare cases, like when talking explicitly about gender identity
+            or translating a text about a nonbinary character without erasing their identity expressed in the original version.
+        </p>
+    </section>
 </template>
