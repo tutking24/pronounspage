9f46a7a28cf9b2b8f7ec2e10c9c80b579c21b0f5
Andrea <andrea@avris.it>
2022-08-06 17:19:46 +0200

    [pl][census] more 2023 preparations, subscriptions

---
+++ server/routes/subscription.js
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import SQL from 'sql-template-strings';
+import {ulid} from "ulid";
+import {handleErrorAsync} from "../../src/helpers";
+import {validateEmail} from "./user";
+
+const router = Router();
+
+router.post('/subscription/subscribe', handleErrorAsync(async (req, res) => {
+    const email = (req.body.email || '').toLowerCase();
+    if (!email || !await validateEmail(email) || !['census'].includes(req.body.type)) {
+        console.log(email, await validateEmail(email), ['census'].includes(req.body.type));
+        return res.status(400).json({error: 'Bad request'});
+    }
+
+    const existing = await req.db.get(SQL`SELECT COUNT(*) as c FROM subscriptions WHERE email = ${email}`);
+
+    if (existing.c === 0) {
+        await req.db.get(SQL`INSERT INTO subscriptions (id, locale, type, email) VALUES
+            (${ulid()}, ${global.config.locale}, ${req.body.type}, ${email})`)
+    }
+
+    return res.json('Subscribed');
+}));
+
+// TODO /api/subscription/unsubscribe?email=andrea@avris.it
+router.get('/subscription/unsubscribe', handleErrorAsync(async (req, res) => {
+    if (!req.query.email) {
+        return res.status(400).json({error: 'Bad request'});
+    }
+
+    await req.db.get(SQL`DELETE FROM subscriptions WHERE email = ${req.query.email}`)
+
+    return res.json('Unsubscribed');
+}));
+
+export default router;
