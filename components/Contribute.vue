234aa9f9305517ac5e6d1a8a80fb500a339ba666
Andrea Vos <andrea@avris.it>
2022-08-16 20:29:01 +0200

    <Contribute> consistent header levels

--- components/Contribute.vue
+++ components/Contribute.vue
@@ -1,18 +1,18 @@
 <template>
     <section>
-        <h2>
+        <h3>
             <Icon v="people-carry"/>
             <T>contact.contribute.header</T>
-        </h2>
+        </h3>
         <p>
             <T>contact.contribute.intro</T>
         </p>
         <div class="row small">
             <div v-for="(icon, area) in areas" class="col-12 col-lg-3">
-                <h3 class="h4 mt-3">
+                <h4 class="h4 mt-3">
                     <Icon :v="icon"/>
                     <T>contact.contribute.{{area}}.header</T>
-                </h3>
+                </h4>
                 <T>contact.contribute.{{area}}.description</T>
             </div>
         </div>
