697dd1824eb297103d55b2f1dbe6b3acfc4232f9
Andrea Vos <andrea@avris.it>
2022-10-11 11:10:53 +0200

    [bug][calendar] fix calendar previews layout

--- routes/calendarDay.vue
+++ routes/calendarDay.vue
@@ -1,5 +1,5 @@
 <template>
-    <Page>
+    <component :is="basic ? 'div' : 'Page'">
     <div v-if="year.eventsByDate[day.toString()] || basic" :class="basic ? 'py-5' : ''">
         <CommunityNav v-if="!basic"/>
 
@@ -51,7 +51,7 @@
         </template>
     </div>
     <NotFound v-else/>
-    </Page>
+    </component>
 </template>
 
 <script>
