f9b4d65f249f96aeb9f612de2baf3a714715a8f6
Andrea Vos <andrea@avris.it>
2022-12-09 20:28:42 +0100

    [profile] <ExpandableList>

---
+++ components/ExpandableList.vue
@@ -0,0 +1,53 @@
+<template>
+    <ul>
+        <li v-for="(el, i) in values">
+            <template v-if="allShown || i < showLimit">
+                <slot v-bind:el="el" v-bind:i="i">
+                    {{ el }}
+                </slot>
+            </template>
+        </li>
+        <li v-if="!allShown && hiddenCount > 0" class="small">
+            <span v-if="static">
+                <Icon v="plus-circle"/>
+                <T :params="{count: hiddenCount}">profile.expendableList.more</T>
+            </span>
+            <a v-else href="#" @click.prevent="allShown = true">
+                <Icon v="plus-circle"/>
+                <T :params="{count: hiddenCount}">profile.expendableList.more</T>
+                <br/>
+                <Icon v=""/>
+                <T>profile.expendableList.show</T>
+            </a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        values: { required: true },
+        limit: { required: true },
+        static: { type: Boolean },
+    },
+    data() {
+        let showLimit = this.values.length;
+        let hiddenCount = 0;
+
+        if (this.values.length > this.limit) {
+            showLimit = this.limit;
+            hiddenCount = this.values.length - this.limit;
+        }
+        if (hiddenCount === 1) {
+            showLimit--;
+            hiddenCount++;
+        }
+
+        return {
+            allShown: false,
+            showLimit,
+            hiddenCount,
+        }
+    },
+}
+</script>
